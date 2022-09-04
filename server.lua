10a539a5b70c0b8ba7af9f6a8a019e369e1ff81a
theenack <90981544+theenack@users.noreply.github.com>
2022-09-04 15:45:39 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 -------------------------
---- BadgerBankRobbery ---
+--- NDBankRobbery ---
 -------------------------
 NDCore = exports["ND_Core"]:GetCoreObject()
 RegisterServerEvent("GiveRewardBank", function()
@@ -30,20 +30,20 @@ end)
 
 --- Server ---
 robberyActive = false
-RegisterNetEvent('BadgerBankRobbery:IsActive')
-AddEventHandler('BadgerBankRobbery:IsActive', function()
+RegisterNetEvent('NDBankRobbery:IsActive')
+AddEventHandler('NDBankRobbery:IsActive', function()
 	-- Check if active or not
 	if robberyActive then
 		-- One is active
-		TriggerClientEvent('BadgerBankRobbery:IsActive:Return', source, true)
+		TriggerClientEvent('NDBankRobbery:IsActive:Return', source, true)
 	else
 		-- One is not active
-		TriggerClientEvent('BadgerBankRobbery:IsActive:Return', source, false)
+		TriggerClientEvent('NDBankRobbery:IsActive:Return', source, false)
 	end
 end)
 
-RegisterNetEvent('BadgerBankRobbery:SetActive')
-AddEventHandler('BadgerBankRobbery:SetActive', function(bool)
+RegisterNetEvent('NDBankRobbery:SetActive')
+AddEventHandler('NDBankRobbery:SetActive', function(bool)
 	robberyActive = bool
 	if bool then
 		Wait((1000 * 60 * config.robberyCooldown)) -- Wait 15 minutes
@@ -54,7 +54,7 @@ end)
 RegisterNetEvent('Print:PrintDebug')
 AddEventHandler('Print:PrintDebug', function(msg)
 	print(msg)
-	TriggerClientEvent('chatMessage', -1, "^7[^1Badger's Scripts^7] ^1DEBUG ^7" .. msg)
+	TriggerClientEvent('chatMessage', -1, "^7[^ND's Scripts^7] ^1DEBUG ^7" .. msg)
 end)
 RegisterNetEvent('PrintBR:PrintMessage')
 AddEventHandler('PrintBR:PrintMessage', function(msg)
